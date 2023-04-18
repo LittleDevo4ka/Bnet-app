ff6105c38b64ccd71846758938f5db3ebceaa7e7
LittleDovo4ka <Dorofey0.8.0@gmail.com>
2023-04-19 01:37:37 +0300

    ADD: HomeFragment markup

--- app/src/main/java/com/example/bnetapp/view/HomeFragment.kt
+++ app/src/main/java/com/example/bnetapp/view/HomeFragment.kt
@@ -1,10 +1,11 @@
 package com.example.bnetapp.view
 
+import android.content.Context
 import android.os.Bundle
+import android.view.*
+import android.view.inputmethod.InputMethodManager
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.example.bnetapp.R
 import com.example.bnetapp.databinding.FragmentHomeBinding
 
@@ -21,6 +22,40 @@ class HomeFragment : Fragment() {
         binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.searchEditText.visibility = View.GONE
+        binding.topAppBarHome.navigationIcon = null
+
+        binding.topAppBarHome.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.search_button -> {
+                    binding.topAppBarHome.menu.findItem(R.id.search_button).isVisible = false
+                    binding.topAppBarHome.menu.findItem(R.id.close_button).isVisible = true
+                    binding.searchEditText.visibility = View.VISIBLE
+                    binding.searchEditText.requestFocus()
+                    val imm = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+                    imm?.showSoftInput(binding.searchEditText, InputMethodManager.SHOW_IMPLICIT)
+                    true
+                }
+                R.id.close_button -> {
+                    binding.topAppBarHome.menu.findItem(R.id.search_button).isVisible = true
+                    binding.topAppBarHome.menu.findItem(R.id.close_button).isVisible = false
+                    binding.searchEditText.visibility = View.GONE
+                    val imm = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager?
+                    imm?.hideSoftInputFromWindow(binding.searchEditText.windowToken, 0)
+                    true
+                }
+                else -> false
+            }
+        }
+
+
+
+
     }
 
 
